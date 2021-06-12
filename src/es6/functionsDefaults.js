d67559ab88d840e4f1a429248fc5c209b89a79b2
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-12 17:55:47 -0500

    :art: Update

---
+++ src/es6/functionsDefaults.js
@@ -0,0 +1,14 @@
+// Old JavaScript ⬇️
+// function newFunction(name, age, country) {
+//   var name = name || 'Zero';
+//   var age = age || 14;
+//   var country = country || 'Perú';
+//   console.log(name, age, country);
+// }
+
+// ECMAScript 6+
+const newFunctionTwo = (name = 'Zero', age = 14, country = 'Perú') =>
+  console.log(name, age, country);
+
+newFunctionTwo();
+newFunctionTwo('UltiRequiem', 28, 'MX');
---
+++ src/es6/stringTemplates.js
@@ -0,0 +1,8 @@
+const hello = 'Hello';
+const world = 'World';
+
+// Old way
+// const epicPhrase = hello + ' ' + world + '.'
+
+const epicPhrase = `${hello} ${world}.`;
+console.log(epicPhrase);
